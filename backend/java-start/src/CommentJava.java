315b2030291af2ef8f146ec74f0d0fc0a2b798b1
JW <jwyang34@gmail.com>
2025-12-06 02:30:27 +0900

    add: 섹션2 실습

---
+++ backend/java-start/src/CommentJava.java
@@ -0,0 +1,13 @@
+public class CommentJava {
+
+    // psvm 치면 자동으로 만들어진다!
+    public static void main() {
+        // sout + enter, shortcut
+        System.out.println("Hello Java1"); // 1 출력
+        // System.out.println("Hello Java2");
+        /*
+        System.out.println("Hello Java3");
+        System.out.println("Hello Java4");
+         */
+    }
+}
