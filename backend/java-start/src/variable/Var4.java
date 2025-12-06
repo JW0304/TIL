512f2a8cf3ab368595bdf639cd650b5aa323b6e8
JW <jwyang34@gmail.com>
2025-12-07 01:50:33 +0900

    add: 섹션3 13강까지 실습

---
+++ backend/java-start/src/variable/Var4.java
@@ -0,0 +1,17 @@
+package variable;
+
+public class Var4 {
+    public static void main(String[] args) {
+        int a, b, c, d;
+
+        a = 1;
+        System.out.println(a);
+
+        b = 2; // 선언과 동시에 초기화
+        System.out.println(b);
+
+        int f = 3, g = 4; // 여러 변수 선언과 동시에 초기화
+        System.out.println(f);
+        System.out.println(g);
+    }
+}
---
+++ backend/java-start/src/variable/Var7.java
@@ -0,0 +1,20 @@
+package variable;
+
+public class Var7 {
+    public static void main(String[] args) {
+        int a = 100; // 정수
+        double b = 10.5; // 실수
+        boolean c = true; // 불리언
+        char d = 'A'; // 문자 하나! 두 개는 안 됨, 홑따옴표 또는 작은따옴표
+        String e = "Hello Java"; // 문자열을 다루는 클래스, 얘만 특별하게 대문자로 시작, 쌍따옴표 또는 큰따옴표
+
+        // int z = "100";
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e); // 윈도우는 Ctrl+D로 한 줄 복사 가능(Duplicate Line)
+
+    }
+}
