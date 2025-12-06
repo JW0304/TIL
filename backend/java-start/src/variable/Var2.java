a4365947ff7591adc4458f5050341d51d0cf37a3
JW <jwyang34@gmail.com>
2025-12-06 23:53:50 +0900

    add: 변수 실습

---
+++ backend/java-start/src/variable/Var2.java
@@ -0,0 +1,14 @@
+package variable;
+
+import java.sql.SQLOutput;
+
+public class Var2 {
+    public static void main(String[] args) {
+        int a;
+        a = 20;
+
+        System.out.println(a);
+        System.out.println(a);
+        System.out.println(a);
+    }
+}
