d9f1a96e1593e351c784d90eb64746a26d566032
JW <jwyang34@gmail.com>
2025-12-07 20:23:20 +0900

    add: 섹션4 20강까지 실습

--- backend/java-start/src/operator/OperatorAdd2.java
+++ backend/java-start/src/operator/OperatorAdd2.java
@@ -6,13 +6,13 @@ public class OperatorAdd2 {
         int b = 0;
 
         b = ++a; // 증감 연산자 -> 대입 연산자: a = a + 1, b = a
-        System.out.println("a = "+ a + ", b = "+ b);
+        System.out.println("a = "+ a + ", b = "+ b); // a = 2, b = 2
 
         a = 1;
         b = 0; // 다시 원래 값으로 돌린다
 
         b = a++; // 대입 연산자 -> 증감 연산자: b = a, a = a + 1
-        System.out.println("a = "+ a + ", b = "+ b);
+        System.out.println("a = "+ a + ", b = "+ b); // a = 2, b = 1
 
         // 증감 연산자: 앞에 있으면 먼저 증감, 뒤에 있으면 마지막에 증감한다
         // ++a 는 a = a + 1 과 같다. 1 증가시키는 것!
