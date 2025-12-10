5a1b814821cb5425da6036d53e224e9431b49a7e
JW <jwyang34@gmail.com>
2025-12-10 21:48:57 +0900

    add: 섹션6 39강까지 실습

---
+++ backend/java-start/src/loop/For1.java
@@ -0,0 +1,10 @@
+package loop;
+
+public class For1 {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 10; i++) {
+            // Ctrl + Shift + Enter 단축키, complete current statement
+            System.out.println(i);
+        }
+    }
+}
